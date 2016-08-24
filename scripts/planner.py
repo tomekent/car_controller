f9087aee81d20e5ef1d48147cf5c97db199d4a33
Tom Kent (Mac) <tomekent@hotmail.com>
2016-08-24 18:53:13 +0100

    gettin tf to give me global postition from odom

--- scripts/planner.py
+++ scripts/planner.py
@@ -4,7 +4,7 @@
 import rospy
 import std_msgs.msg
 from nav_msgs.msg import Odometry
-from geometry_msgs.msg import Twist, Pose
+from geometry_msgs.msg import Twist, Pose, TwistStamped, PoseStamped
 from sensor_msgs.msg import LaserScan
 import math
 import tf
@@ -15,9 +15,9 @@ class pathPlanner():
 	def __init__(self):
 		self.update_rate = 60
 
-		self.origin_x = 0.0
-		self.origin_y = 0.0
-		self.origin_yaw = 0.0
+		self.origin_x = 5.5
+		self.origin_y = 0.5
+		self.origin_yaw = math.pi/2
 		self.scanData = []
 		self.pioneer_nom_v = 0.5
 		self.pioneer_travel_dist = 0.0
@@ -25,10 +25,10 @@ class pathPlanner():
 		self.tf_listener = tf.TransformListener()
 		self.br = tf.TransformBroadcaster()
 		self.brglobal = tf.TransformBroadcaster()
-		self.current_pose_odom = Pose()
-		self.current_vel_odom = Twist()
+		self.current_pose_odom = PoseStamped()
+		self.current_vel_odom = TwistStamped()
 
-		self.init_pose = Pose()
+		self.init_pose = PoseStamped()
 
 		self.init_flag = True
 		self.obs_buffer = 0.5 # avoidance distance m.
@@ -49,12 +49,13 @@ class pathPlanner():
 
 	def odometry_callback(self, data):
 		if self.init_flag: # set the initial odometry location
-			self.init_pose.position = data.pose.pose.position
-			self.init_pose.orientation = data.pose.pose.orientation
+			self.init_pose.header.frame_id  = 'odom_initialised'
+			self.init_pose.pose.position = data.pose.pose.position
+			self.init_pose.pose.orientation = data.pose.pose.orientation
 			self.init_flag = False
-			euler = euler_from_quaternion([self.init_pose.orientation.x, self.init_pose.orientation.y, self.init_pose.orientation.z, self.init_pose.orientation.w])
+			euler = euler_from_quaternion([self.init_pose.pose.orientation.x, self.init_pose.pose.orientation.y, self.init_pose.pose.orientation.z, self.init_pose.pose.orientation.w])
 			yaw = euler[2]
-			print("Initial pose: x: %2.2f, y: %2.2f, theta: %2.2f" %(self.init_pose.position.x, self.init_pose.position.y, yaw))
+			print("Initial pose: x: %2.2f, y: %2.2f, theta: %2.2f" %(self.init_pose.pose.position.x, self.init_pose.pose.position.y, yaw))
 
 		q = quaternion_from_euler(self.origin_yaw, 0.,0.,'rzyx')
 		self.br.sendTransform((self.origin_x, self.origin_y, 0.),
@@ -65,20 +66,20 @@ class pathPlanner():
   						"odom_initialised",
   						"global")
 
-		self.brglobal.sendTransform((self.init_pose.position.x, self.init_pose.position.y, self.init_pose.position.z),
-						 (self.init_pose.orientation.x, self.init_pose.orientation.y, self.init_pose.orientation.z, self.init_pose.orientation.w),
+		self.brglobal.sendTransform((self.init_pose.pose.position.x, self.init_pose.pose.position.y, self.init_pose.pose.position.z),
+						 (self.init_pose.pose.orientation.x, self.init_pose.pose.orientation.y, self.init_pose.pose.orientation.z, self.init_pose.pose.orientation.w),
 		# br.sendTransform((data.pose.pose.position.x, data.pose.pose.position.y, data.pose.pose.position.z),
   						# (data.pose.pose.orientation.x, data.pose.pose.orientation.y, data.pose.pose.orientation.z, data.pose.pose.orientation.w),
   						rospy.Time.now(),
   						"odom",
   						"odom_initialised")
-
-		self.current_pose_odom.position = data.pose.pose.position
-		self.current_pose_odom.orientation = data.pose.pose.orientation
-		self.current_vel_odom.linear = data.twist.twist.linear
-		self.current_vel_odom.angular = data.twist.twist.angular
-
-
+		self.current_pose_odom.header.frame_id  = 'odom'
+		self.current_pose_odom.header.stamp = rospy.Time();
+		self.current_pose_odom.pose.position = data.pose.pose.position
+		self.current_pose_odom.pose.orientation = data.pose.pose.orientation
+		self.current_vel_odom.twist.linear = data.twist.twist.linear
+		self.current_vel_odom.twist.angular = data.twist.twist.angular
+		
 		# print self.current_vel.linear.x
 
 	def get_nearest_reading(self):
@@ -94,7 +95,7 @@ class pathPlanner():
 		ob_dist_min = self.obs_buffer
 
 		nom_vel = self.pioneer_nom_v
-		current_vel = self.current_vel_odom.linear.x
+		current_vel = self.current_vel_odom.twist.linear.x
 		acc = 4. #m/s/s
 		decc = -2. #m/s/s
 		max_decc = -4.
@@ -129,6 +130,17 @@ class pathPlanner():
 
 		return new_vel
 
+	def odom_to_global(self, odom):
+		try:
+			(self.trans, rot) = self.tf_listener.lookupTransform('/odom', '/global', rospy.Time(0))
+		except:
+			print "odom_to_global exception"
+			pass
+
+		object_pose = self.tf_listener.transformPose("/global", self.current_pose_odom)
+		print object_pose
+
+
 
 def set_cmd_ctrl(cmd, speed, steer):
 	cmd.linear.x = speed
@@ -156,13 +168,37 @@ def main():
 
 	while not rospy.is_shutdown():
 
+		
+		current_pose_global = PoseStamped()
+		# print planner.current_pose_odom
+		now = rospy.Time.now()
+		# print (planner.current_pose_odom.header.stamp)
+		# planner.tf_listener.waitForTransform("/global", "/odom", now, rospy.Duration(4.0))
+		# print (planner.current_pose_odom.header.stamp)
 		try:
 			(trans, rot)=planner.tf_listener.lookupTransform('/odom', '/global',rospy.Time(0))
-			print trans
-			print rot
+			current_pose_odom = planner.current_pose_odom
+			current_pose_init = planner.init_pose
+			current_pose_init = planner.tf_listener.transformPose('/odom_initialised', current_pose_odom)
+			current_pose_global = planner.tf_listener.transformPose('/global', current_pose_odom)
+			# print trans
+			# print rot
+			euler_odom = euler_from_quaternion([current_pose_odom.pose.orientation.x, current_pose_odom.pose.orientation.y, current_pose_odom.pose.orientation.z, current_pose_odom.pose.orientation.w])
+			euler_init = euler_from_quaternion([current_pose_init.pose.orientation.x, current_pose_init.pose.orientation.y, current_pose_init.pose.orientation.z, current_pose_init.pose.orientation.w])
+			euler_global = euler_from_quaternion([current_pose_global.pose.orientation.x, current_pose_global.pose.orientation.y, current_pose_global.pose.orientation.z, current_pose_global.pose.orientation.w])
+			print("current odom pose: x: %2.2f, y: %2.2f, heading: %2.2f" %(current_pose_odom.pose.position.x, current_pose_odom.pose.position.y , euler_odom[2]))
+			print("current init pose: x: %2.2f, y: %2.2f, heading: %2.2f" %(current_pose_init.pose.position.x, current_pose_init.pose.position.y , euler_init[2]))
+			print("current global pose: x: %2.2f, y: %2.2f, heading: %2.2f" %(current_pose_global.pose.position.x, current_pose_global.pose.position.y , euler_global[2]))
 		except:
 			print "pass"
 			pass
+
+	# (trans,rot) = planner.tf_listener.lookupTransform("odom", "global", rospy.Time(0))
+		# print trans
+		# print rot
+		# object_pose = planner.tf_listener.transformPose("odom", planner.current_pose_odom)
+		# print object_pose
+
 		#### Do the pioneer Logic ####
 		nearest_reading = planner.get_nearest_reading()
 
@@ -205,14 +241,14 @@ def main():
 
 			
 		# rospy.loginfo("publishing a command to Pioneer")
-		planner.pioneer_pub.publish(pioneer_cmd)
+		# planner.pioneer_pub.publish(pioneer_cmd)
 		
 		# rospy.loginfo("publishing a command to rccar: {v: %2.2f, w: %2.2f}" %(rccar_cmd.linear.x, rccar_cmd.angular.x))
 		# planner.rccar_pub.publish(rccar_cmd)
 
 		# update the distances based on the speeds
 		
-		planner.pioneer_travel_dist += dt*planner.current_vel_odom.linear.x
+		planner.pioneer_travel_dist += dt*planner.current_vel_odom.twist.linear.x
 		# rospy.loginfo("pioneer has travelled: %2.4f meters" %planner.pioneer_travel_dist)		
 
 		# planner.rccar_travel_dist += dt*rccar_speed
